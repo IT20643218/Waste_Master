d796fea7242c1300dcaa04e085fd5ad403213cb6
Sumedha Lakmal Bandaranayaka <it20643218@my.sliit.lk>
2022-04-27 12:11:09 +0530

    Add new function

--- app/src/main/java/com/example/wastemaster/CleaningActivity2.java
+++ app/src/main/java/com/example/wastemaster/CleaningActivity2.java
@@ -2,13 +2,29 @@ package com.example.wastemaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CleaningActivity2 extends AppCompatActivity {
+
+    //create reference
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cleaning2);
+
+        button = findViewById(R.id.btact_id1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),CreateBin.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/wastemaster/CreateBin.java
+++ app/src/main/java/com/example/wastemaster/CreateBin.java
@@ -2,13 +2,31 @@ package com.example.wastemaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateBin extends AppCompatActivity {
+
+    private Button select_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_bin);
+
+        select_btn = findViewById(R.id.btn_select);
+        select_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:6.927079"));
+                Intent chooser = Intent.createChooser(intent,"Lauch Maps");
+                startActivity(chooser);
+            }
+        });
     }
+
 }
