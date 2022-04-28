1d15a6df66562ee645a22a245f072cc7c1731ff0
Sumedha Lakmal Bandaranayaka <it20643218@my.sliit.lk>
2022-04-28 14:09:44 +0530

    Add List View

--- app/src/main/java/com/example/wastemaster/CleaningActivity2.java
+++ app/src/main/java/com/example/wastemaster/CleaningActivity2.java
@@ -10,16 +10,27 @@ import android.widget.Button;
 public class CleaningActivity2 extends AppCompatActivity {
 
     //create reference
-    Button button;
+    Button button1;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cleaning2);
 
-        button = findViewById(R.id.btact_id1);
+        button1 = findViewById(R.id.btact_id1);
+        button2 = findViewById(R.id.btact_id4);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        //button worksrports
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(getApplicationContext(),WorkReport.class);
+            }
+        });
+
+        //button cleaning officer
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(),CreateBin.class);
--- app/src/main/java/com/example/wastemaster/WorkReport.java
+++ app/src/main/java/com/example/wastemaster/WorkReport.java
@@ -1,14 +1,72 @@
 package com.example.wastemaster;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class WorkReport extends AppCompatActivity {
+
+    ListView listView;
+
+    String title[] = {"Location:Boralesgamuwa","Location:Dehiwal","Location:Bellanwila","Location:Maharagama",
+    "Location:Nugegoda"};
+
+    String description[]={"Category:Plastic,Food Waste,Glass","Category:Glass,Food Waste","Category:Food Waste","Category:Plastic",
+            "Category:Glass"};
+
+    int image[] ={R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_report);
+
+        listView = findViewById(R.id.listview);
+        CustomeAdapter adapter = new CustomeAdapter(this,title,image,description);
+        listView.setAdapter(adapter);
+    }
+}
+class CustomeAdapter extends ArrayAdapter<String>{
+
+    Context context;
+    int[] images;
+    String[] title;
+    String[] des;
+
+    CustomeAdapter(Context context,String[] title,int[] images,String[] des){
+        super(context,R.layout.singal_row,R.id.textView,title);
+        this.context = context;
+        this.images = images;
+        this.title = title;
+        this.des = des;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View row = inflater.inflate(R.layout.singal_row,parent,false);
+
+        ImageView imageView = row.findViewById(R.id.imageView7);
+        TextView titleview = row.findViewById(R.id.textView);
+        TextView descriptionview = row.findViewById(R.id.textView2);
+
+        imageView.setImageResource(images[position]);
+        titleview.setText(title[position]);
+        descriptionview.setText(des[position]);
+
+        return row;
+
     }
 }
